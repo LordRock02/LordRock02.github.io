49c46d35721836b4d8a8a8dfd8669eb578522eba
roger gomez <79542018+LordRock02@users.noreply.github.com>
2023-05-31 19:40:11 -0500

    send.php

--- send.php
+++ send.php
@@ -14,7 +14,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'irogergt02@gmail.com';
+$para = 'rogergt02@gmail.com';
 
 mail($para, $subject, utf8_decode($mensaje), $header);
 
